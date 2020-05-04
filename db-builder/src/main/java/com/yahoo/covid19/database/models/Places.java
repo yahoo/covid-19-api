0d9407f537de1117f46a52c265ac7fb094a86d36
Chandrasekar Rajasekar <chandrasekar.rajasekar@oath.com>
2020-05-04 12:26:47 -0500

    remove unused import

--- db-builder/src/main/java/com/yahoo/covid19/database/models/Places.java
+++ db-builder/src/main/java/com/yahoo/covid19/database/models/Places.java
@@ -18,7 +18,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
