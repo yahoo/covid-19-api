c674b1c4f8dedb4ae1b9edd78c5a3b3621798cb1
Chandrasekar Rajasekar <chandrasekar.rajasekar@oath.com>
2020-05-18 10:45:04 -0500

    Add foreign key as additional attribute field in HealthRecords

--- webservice/src/main/java/com/yahoo/covid19/models/LatestHealthRecords.java
+++ webservice/src/main/java/com/yahoo/covid19/models/LatestHealthRecords.java
@@ -10,6 +10,7 @@ import com.yahoo.elide.annotation.Include;
 import java.util.Date;
 import java.util.UUID;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -26,6 +27,9 @@ public class LatestHealthRecords {
     @JoinColumn(name = "regionId")
     @OneToOne
     private Place place;
+
+    @Column(name = "regionId", insertable = false, updatable = false)
+    private String placeId;
 
     private String label;
     private String wikiId;
