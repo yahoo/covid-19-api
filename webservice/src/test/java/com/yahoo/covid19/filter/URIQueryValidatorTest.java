d9e7ad98a9d1cb42f3b6b1d3a6720502518d3798
Chandrasekar-Rajasekar <chandrasekar.rajasekar@gmail.com>
2020-05-19 17:14:15 -0500

    Added the whitelist of url following parent child model (#24)
    Co-authored-by: Chandrasekar Rajasekar <chandrasekar.rajasekar@oath.com>

--- webservice/src/test/java/com/yahoo/covid19/filter/URIQueryValidatorTest.java
+++ webservice/src/test/java/com/yahoo/covid19/filter/URIQueryValidatorTest.java
@@ -32,15 +32,15 @@ import java.util.List;
 public class URIQueryValidatorTest {
     private List<String> strRules = Arrays.asList(
             "/api/json/v1/healthRecords?filter=referenceDate=={{DATE}}",
-            "/api/json/v1/healthRecords?filter=referenceDate=in=({{DATE_LIST}})&includes=county",
-            "/api/json/v1/county?filter=label=={{TYPE_AHEAD_CHARACTERS}}",
+            "/api/json/v1/healthRecords?filter=referenceDate=in=({{DATE_LIST}})&includes=place",
+            "/api/json/v1/places?filter=label=={{TYPE_AHEAD_CHARACTERS}}",
+            "/api/json/v1/places?filter=label1=={{TYPE_AHEAD_CHARACTERS}};id=={{ALPHA_NUMERIC}}",
             "/api/json/v1/metadata?fields[metadata]={{ALPHA_NUMERIC}},{{ALPHA_NUMERIC}}",
-            "/api/json/v1/states",
-            "/api/json/v1/country?filter=wikiId=={{TYPE_AHEAD_CHARACTERS}}{{PAGINATION}}",
-            "/api/json/v1/country?{{PAGINATION}}",
-            "/api/json/v1/state?fields[state]={{FIELD_ATTRIBUTES}}{{PAGINATION}}",
-            "/api/json/v1/country?cache={{[0|1]?}}",
-            "/api/json/v1/country?cache={{[\\?)}}"             // Not a valid regex. Should be discarded.
+            "/api/json/v1/places",
+            "/api/json/v1/places?filter=wikiId=={{TYPE_AHEAD_CHARACTERS}}{{PAGINATION}}",
+            "/api/json/v1/places?{{PAGINATION}}",
+            "/api/json/v1/places?fields[places]={{FIELD_ATTRIBUTES}}{{PAGINATION}}",
+            "/api/json/v1/places?page={{[\\?)}}"             // Not a valid regex. Should be discarded.
     );
     private URIQueryValidator queryValidator;
 
@@ -52,15 +52,15 @@ public class URIQueryValidatorTest {
     @Test
     public void testRegex() {
         Assertions.assertEquals( "[" +
-                "\\Q/api/json/v1/healthRecords?filter=referenceDate==\\E" + "'\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}Z'" + ", " +
-                "\\Q/api/json/v1/healthRecords?filter=referenceDate=in=(\\E" + "(?:'\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}Z',?)+" + "\\Q)&includes=county\\E" + ", " +
-                "\\Q/api/json/v1/county?filter=label==\\E" + "'[^\u0000-\u001F']+'" + ", " +
+                "\\Q/api/json/v1/healthRecords?filter=referenceDate==\\E" + "'?\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}Z'?" + ", " +
+                "\\Q/api/json/v1/healthRecords?filter=referenceDate=in=(\\E" + "(?:'\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}Z',?)+" + "\\Q)&includes=place\\E" + ", " +
+                "\\Q/api/json/v1/places?filter=label==\\E" + "'[^\u0000-\u001F]+'" + ", " +
+                "\\Q/api/json/v1/places?filter=label1==\\E" + "'[^\u0000-\u001F]+'" + "\\Q;id==\\E" + "'[\\w\\-]+'" + ", " +
                 "\\Q/api/json/v1/metadata?fields[metadata]=\\E" + "'[\\w\\-]+'" + "\\Q,\\E" + "'[\\w\\-]+'" + ", " +
-                "\\Q/api/json/v1/states\\E" + ", " +
-                "\\Q/api/json/v1/country?filter=wikiId==\\E" + "'[^\u0000-\u001F']+'" + "(?:&?page\\[\\w+\\]=\\d+)*" + ", " +
-                "\\Q/api/json/v1/country?\\E" + "(?:&?page\\[\\w+\\]=\\d+)*" + ", " +
-                "\\Q/api/json/v1/state?fields[state]=\\E" + "(?:(?!country|state|states|county|counties|&)[\\w,])*" + "(?:&?page\\[\\w+\\]=\\d+)*" + ", " +
-                "\\Q/api/json/v1/country?cache=\\E" + "[0|1]?" +
+                "\\Q/api/json/v1/places\\E" + ", " +
+                "\\Q/api/json/v1/places?filter=wikiId==\\E" + "'[^\u0000-\u001F]+'" + "(?:&?page\\[\\w+\\]=\\d+)*" + ", " +
+                "\\Q/api/json/v1/places?\\E" + "(?:&?page\\[\\w+\\]=\\d+)*" + ", " +
+                "\\Q/api/json/v1/places?fields[places]=\\E" + "(?:(?!place|places|&)[\\w,])*" + "(?:&?page\\[\\w+\\]=\\d+)*" +
                 "]",
                 queryValidator.getRegexRules().toString()
         );
@@ -71,11 +71,12 @@ public class URIQueryValidatorTest {
         String uri1 = "/api/json/v1/healthRecords?filter=referenceDate=='2020-04-03T00:00Z'";
         Assertions.assertTrue(queryValidator.validate(uri1));
 
-        ////Invalid Records
-
         // No single quotes around date
         String uri2 = "/api/json/v1/healthRecords?filter=referenceDate==2020-04-03T00:00Z";
-        Assertions.assertFalse(queryValidator.validate(uri2));
+        Assertions.assertTrue(queryValidator.validate(uri2));
+
+
+        ////Invalid Records
 
         // Invalid date format yyyy-mm-dd
         String uri3 = "/api/json/v1/healthRecords?filter=referenceDate=='2020-4-3T00:00Z'";
@@ -105,8 +106,8 @@ public class URIQueryValidatorTest {
         String uri9 = "/api/json/v1/healthRecords?filter=referenceDate==('2020-04-03T00:00')";
         Assertions.assertFalse(queryValidator.validate(uri9));
 
-        // Root Country is not whitelisted uri
-        String uri10 = "/api/json/v1/country?filter=referenceDate=='2020-04-03T00:00'";
+        // Root latestHealthRecords is not whitelisted uri
+        String uri10 = "/api/json/v1/latestHealthRecords?filter=referenceDate=='2020-04-03T00:00'";
         Assertions.assertFalse(queryValidator.validate(uri10));
 
         // Empty string
@@ -116,10 +117,10 @@ public class URIQueryValidatorTest {
 
     @Test
     public void testURIWithDATE_LIST() {
-        String uri1 = "/api/json/v1/healthRecords?filter=referenceDate=in=('2020-04-03T00:00Z')&includes=county";
+        String uri1 = "/api/json/v1/healthRecords?filter=referenceDate=in=('2020-04-03T00:00Z')&includes=place";
         Assertions.assertTrue(queryValidator.validate(uri1));
 
-        String uri2 = "/api/json/v1/healthRecords?filter=referenceDate=in=('2020-04-03T00:00Z','2020-04-05T00:00Z')&includes=county";
+        String uri2 = "/api/json/v1/healthRecords?filter=referenceDate=in=('2020-04-03T00:00Z','2020-04-05T00:00Z')&includes=place";
         Assertions.assertTrue(queryValidator.validate(uri2));
 
 
@@ -134,116 +135,114 @@ public class URIQueryValidatorTest {
         Assertions.assertFalse(queryValidator.validate(uri4));
 
         // No Single Quotes
-        String uri5 = "/api/json/v1/healthRecords?filter=referenceDate=in=('2020-04-03T00:00Z,2020-04-05T00:00Z')&includes=county";
+        String uri5 = "/api/json/v1/healthRecords?filter=referenceDate=in=('2020-04-03T00:00Z,2020-04-05T00:00Z')&includes=place";
         Assertions.assertFalse(queryValidator.validate(uri5));
 
         // Invalid date format Thh:mmZ
-        String uri6 = "/api/json/v1/healthRecords?filter=referenceDate=in=('2020:04:03T00:00Z','2020-04-05T00:00Z')&includes=county";
+        String uri6 = "/api/json/v1/healthRecords?filter=referenceDate=in=('2020:04:03T00:00Z','2020-04-05T00:00Z')&includes=place";
         Assertions.assertFalse(queryValidator.validate(uri6));
 
         // Invalid time format Thh:mmZ
-        String uri7 = "/api/json/v1/healthRecords?filter=referenceDate=in=('2020-04-03T00:00Z','2020-04-05T00=00Z')&includes=county";
+        String uri7 = "/api/json/v1/healthRecords?filter=referenceDate=in=('2020-04-03T00:00Z','2020-04-05T00=00Z')&includes=place";
         Assertions.assertFalse(queryValidator.validate(uri7));
 
         // Date and Time not delimited
-        String uri8 = "/api/json/v1/healthRecords?filter=referenceDate=in=('2020-04-03T00:00Z','2020-04-0500:00Z')&includes=county";
+        String uri8 = "/api/json/v1/healthRecords?filter=referenceDate=in=('2020-04-03T00:00Z','2020-04-0500:00Z')&includes=place";
         Assertions.assertFalse(queryValidator.validate(uri8));
 
         // Date and Time not delimited
-        String uri9 = "/api/json/v1/healthRecords?filter=referenceDate=in=('2020-04-03T00:00','2020-04-05T00:00Z')&includes=county";
+        String uri9 = "/api/json/v1/healthRecords?filter=referenceDate=in=('2020-04-03T00:00','2020-04-05T00:00Z')&includes=place";
         Assertions.assertFalse(queryValidator.validate(uri9));
 
         // DateTime is not enclose in brackets
-        String uri10 = "/api/json/v1/healthRecords?filter=referenceDate=in='2020-04-03T00:00Z','2020-04-05T00:00Z'&includes=county";
+        String uri10 = "/api/json/v1/healthRecords?filter=referenceDate=in='2020-04-03T00:00Z','2020-04-05T00:00Z'&includes=place";
         Assertions.assertFalse(queryValidator.validate(uri10));
 
         // Root Country is not whitelisted uri
-        String uri11 = "/api/json/v1/state?filter=referenceDate=in=('2020-04-03T00:00Z','2020-04-05T00:00Z')&includes=county";
+        String uri11 = "/api/json/v1/state?filter=referenceDate=in=('2020-04-03T00:00Z','2020-04-05T00:00Z')&includes=place";
         Assertions.assertFalse(queryValidator.validate(uri11));
 
         // Empty list
-        String uri12 = "/api/json/v1/healthRecords?filter=referenceDate=in=()&includes=county";
+        String uri12 = "/api/json/v1/healthRecords?filter=referenceDate=in=()&includes=place";
         Assertions.assertFalse(queryValidator.validate(uri12));
     }
 
     @Test
-    public void testFilter() {
-        String uri1 = "/api/json/v1/county?filter=label=='*In*'";
+    public void testTypeAheadfilter() {
+        String uri1 = "/api/json/v1/places?filter=label=='*In*'";
         Assertions.assertTrue(queryValidator.validate(uri1));
 
-        String uri2 = "/api/json/v1/county?filter=label=='GO*'";
+        String uri2 = "/api/json/v1/places?filter=label=='GO*'";
         Assertions.assertTrue(queryValidator.validate(uri2));
 
-        String uri3 = "/api/json/v1/county?filter=label=='US'";
+        String uri3 = "/api/json/v1/places?filter=label=='US'";
         Assertions.assertTrue(queryValidator.validate(uri3));
 
-        String uri4 = "/api/json/v1/county?filter=label=='*Nation'";
+        String uri4 = "/api/json/v1/places?filter=label=='*Nation'";
         Assertions.assertTrue(queryValidator.validate(uri4));
 
-        // Cannot conatin quotes
-        String uri5 = "/api/json/v1/county?filter=label=='23'45'";
-        Assertions.assertFalse(queryValidator.validate(uri5));
+        String uri5 = "/api/json/v1/places?filter=label=='23'45'";
+        Assertions.assertTrue(queryValidator.validate(uri5));
 
         // cannot be empty
-        String uri6 = "/api/json/v1/county?filter=label==";
+        String uri6 = "/api/json/v1/places?filter=label==";
         Assertions.assertFalse(queryValidator.validate(uri6));
 
         // invalid unicode
-        String uri7 = "/api/json/v1/county?filter=label=='wo\u0005rd'";
+        String uri7 = "/api/json/v1/places?filter=label=='wo\u0005rd'";
         Assertions.assertFalse(queryValidator.validate(uri7));
 
     }
 
     @Test
     public void testNoParameter() {
-        String uri1 = "/api/json/v1/states";
+        String uri1 = "/api/json/v1/places";
         Assertions.assertTrue(queryValidator.validate(uri1));
 
-        String uri2 = "/api/json/v1/states?filter=id==123";
+        String uri2 = "/api/json/v1/places?filter=id==123";
         Assertions.assertFalse(queryValidator.validate(uri2));
 
     }
 
     @Test
     public void testUnicodeCharacters() {
-        String uri1 = "/api/json/v1/country?filter=wikiId=='New_York\\,_(state)'&page[offset]=32";
+        String uri1 = "/api/json/v1/places?filter=wikiId=='New_York\\,_(state)'&page[offset]=32";
         Assertions.assertTrue(queryValidator.validate(uri1));
 
-        String uri2 = "/api/json/v1/country?filter=wikiId=='asd\u09fe\u0020'&page[offset]=32&page[limit]=10";
+        String uri2 = "/api/json/v1/places?filter=wikiId=='asd\u09fe\u0020'&page[offset]=32&page[limit]=10";
         Assertions.assertTrue(queryValidator.validate(uri2));
 
-        String uri3 = "/api/json/v1/country?filter=wikiId=='こんにちは'&page[offset]=32";
+        String uri3 = "/api/json/v1/places?filter=wikiId=='こんにちは'&page[offset]=32";
         Assertions.assertTrue(queryValidator.validate(uri3));
 
 
-        String uri4 = "/api/json/v1/country?filter=wikiId=='\u001f'";
+        String uri4 = "/api/json/v1/places?filter=wikiId=='\u001f'";
         Assertions.assertFalse(queryValidator.validate(uri4));
 
-        String uri5 = "/api/json/v1/country?filter=wikiId=='\u0034asf\u642f\u000023'";
+        String uri5 = "/api/json/v1/places?filter=wikiId=='\u0034asf\u642f\u000023'";
         Assertions.assertFalse(queryValidator.validate(uri5));
 
-        //Quotes are not allowed
-        String uri6 = "/api/json/v1/country?filter=wikiId=='Label's'&page[offset]=32";
-        Assertions.assertFalse(queryValidator.validate(uri6));
+        String uri6 = "/api/json/v1/places?filter=wikiId=='Label's'&page[offset]=32";
+        Assertions.assertTrue(queryValidator.validate(uri6));
     }
 
     @Test
     public void testPagination() {
-        String uri1 = "/api/json/v1/country?page[limit]=10";
+        String uri1 = "/api/json/v1/places?page[limit]=10";
         Assertions.assertTrue(queryValidator.validate(uri1));
 
-        String uri2 = "/api/json/v1/country?page[number]=30&page[offset]=02";
+        String uri2 = "/api/json/v1/places?page[number]=30&page[offset]=02";
         Assertions.assertTrue(queryValidator.validate(uri2));
 
-        String uri3 = "/api/json/v1/country?";
+        String uri3 = "/api/json/v1/places?";
         Assertions.assertTrue(queryValidator.validate(uri3));
 
         // page only takes number
-        String uri4 = "/api/json/v1/country?page[size]=ten";
+        String uri4 = "/api/json/v1/places?page[size]=ten";
         Assertions.assertFalse(queryValidator.validate(uri4));
 
         // page does not have parameter
-        String uri5 = "/api/json/v1/country?page=10";
+        String uri5 = "/api/json/v1/places?page=10";
         Assertions.assertFalse(queryValidator.validate(uri5));
 
     }
@@ -273,35 +272,61 @@ public class URIQueryValidatorTest {
 
     @Test
     public void testFieldAttributes() {
-        String uri1 = "/api/json/v1/state?fields[state]=id,label";
+        String uri1 = "/api/json/v1/places?fields[places]=id,label";
         Assertions.assertTrue(queryValidator.validate(uri1));
 
-        String uri2 = "/api/json/v1/state?fields[state]=id,label,numCases,wikiId";
+        String uri2 = "/api/json/v1/places?fields[places]=id,label,numCases,wikiId";
         Assertions.assertTrue(queryValidator.validate(uri2));
 
         //Space not expected
-        String uri3 = "/api/json/v1/state?fields[state]=id, numCases,wikiId";
+        String uri3 = "/api/json/v1/places?fields[places]=id, numCases,wikiId";
         Assertions.assertFalse(queryValidator.validate(uri3));
 
         //relationship not expected
-        String uri4 = "/api/json/v1/state?fields[state]=id,wikiId,country";
+        String uri4 = "/api/json/v1/places?fields[places]=id,wikiId,place";
         Assertions.assertFalse(queryValidator.validate(uri4));
 
         //With Page
 
-        String uri5 = "/api/json/v1/state?fields[state]=id,label&page[limit]=20";
+        String uri5 = "/api/json/v1/places?fields[places]=id,label&page[limit]=20";
         Assertions.assertTrue(queryValidator.validate(uri5));
 
-        String uri6 = "/api/json/v1/state?fields[state]=id,label,numCases,wikiId&page[limit]=20";
+        String uri6 = "/api/json/v1/places?fields[places]=id,label,numCases,wikiId&page[limit]=20";
         Assertions.assertTrue(queryValidator.validate(uri6));
 
         //Space not expected
-        String uri7 = "/api/json/v1/state?fields[state]=id,numCases,wikiId &page[limit]=20";
+        String uri7 = "/api/json/v1/places?fields[places]=id,numCases,wikiId &page[limit]=20";
         Assertions.assertFalse(queryValidator.validate(uri7));
 
         //relationship not expected
-        String uri8 = "/api/json/v1/state?fields[state]=id,wikiId,country&page[limit]=20";
+        String uri8 = "/api/json/v1/places?fields[places]=id,wikiId,places&page[limit]=20";
         Assertions.assertFalse(queryValidator.validate(uri8));
+    }
+
+    @Test
+    public void testSpecialCharacters() {
+        String uri1 = "/api/json/v1/places?filter=label=='Prince_George%27s_County'";
+        Assertions.assertTrue(queryValidator.validate(uri1));
+
+        String uri2 = "/api/json/v1/places?filter=label=='Prince_George's_County'";
+        Assertions.assertTrue(queryValidator.validate(uri2));
+
+        String uri3 = "/api/json/v1/places?filter=label=='Prince_George\"s_County'";
+        Assertions.assertTrue(queryValidator.validate(uri3));
+
+        // junk filter value
+        String uri4 = "/api/json/v1/places?filter=label1=='Prince_George\\';s_C'o'unty';id=='op'';id=='id13ds'";
+        Assertions.assertTrue(queryValidator.validate(uri4));
+
+        // junk filter value
+        String uri5 = "/api/json/v1/places?filter=label1=='Prince_George'''''';id=='id13ds'";
+        Assertions.assertTrue(queryValidator.validate(uri5));
+
+
+        // invalid
+        String uri6 = "/api/json/v1/places?filter=label1=='Prince_George'p;';;id=='id13ds'";
+        Assertions.assertFalse(queryValidator.validate(uri6));
+
     }
 
 }
